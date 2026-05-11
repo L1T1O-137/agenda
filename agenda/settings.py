a283fac614e86e97c9783b404a8a6953a16b61a9
L1T1O-137 <basillisky@gmail.com>
2026-05-11 20:32:28 -0300

    mandando emails legais

--- agenda/settings.py
+++ agenda/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import EMAIL_BACKEND
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +125,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'basillisky@gmail.com'
+EMAIL_HOST_PASSWORD = 'eyzeixjlunfozkua'
+DEFAULT_FROM_EMAIL = 'Lavacar'
--- agendamentos/views.py
+++ agendamentos/views.py
@@ -2,12 +2,15 @@ from urllib import request
 
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.db import transaction
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from produtos.models import Produto
 from servicos.models import ProdutosServico
 from .forms import AgendamentoListForm, AgendamentoModelForm, AgendamentosServicoInLine
 from .models import Agendamento, OrdemServicos
@@ -141,9 +144,35 @@ class AgendamentoExibir(DetailView):
                         if ordem.servico.produtos:
                             produto_servico = ProdutosServico.objects.filter(servico=ordem.servico)
                             for item in produto_servico:
-                                produto = ProdutosServico.objects.filter(servico=item.servico)
+                                produto = Produto.objects.get(pk=item.produto.pk)
                                 produto.quantidade -= item.quantidade
                                 produto.save()
                 agendamento.status = 'F'
                 agendamento.save()
+                self.enviar_email(agendamento)
         return agendamento
+
+    def enviar_email(self, agendamento):
+        email = []
+        email.append(agendamento.cliente.email)
+        descricao = []
+        for servico in agendamento.ordem_servicos_agendamento.all():
+            descricao.append(f'{servico} - {servico.preco} ({servico.get_situacao_display()})')
+
+        dados = {'cliente': agendamento.cliente.nome,
+                 'horario': agendamento.horario,
+                 'funcionario': agendamento.funcionario.nome,
+                 'descricao': descricao,
+                 'valor': agendamento.valor, }
+
+        texto_email = render_to_string('emails/texto_email.txt', dados)
+        html_email = render_to_string('emails/texto_email.html', dados)
+        send_mail(subject='Lavacar - Serviço concluído',
+                  message=texto_email,
+                  from_email='basillisky@gmail.com',
+                  recipient_list=email,
+                  html_message=html_email,
+                  fail_silently=False,
+                  )
+        return redirect('agendamentos')
+
